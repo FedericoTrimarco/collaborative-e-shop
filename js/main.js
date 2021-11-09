24afe0ef8760b86c0b6a1df92bc5d24e39665ab4
FedericoTrimarco <trimarcofederico98@gmail.com>
2021-11-09 15:08:29 +0100

    add styling setup

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const products = [
         id: 1,
         title: 'Mac Mini',
         subtitle: 'Nuovi muscoli. Nuovi traguardi.',
-        url: 'mac_mini.jpg',
+        url: './img/mac_mini (1).jpg',
         price: 1999.99,
         category: 'laptop',
         description: 'Il nostro computer desktop più versatile si lancia in una nuova dimensione. Mac mini ora ha il rivoluzionario chip Apple M1, CPU fino a 3 volte più rapida, grafica fino a 6 volte più scattante e il Neural Engine più evoluto, che assicura performance fino a 15 volte migliori per il machine learning. Preparati a lavorare, giocare e creare con una velocità e una potenza che prima d’ora non avresti mai immaginato.',
@@ -13,7 +13,7 @@ const products = [
         id: 2,
         title: 'MacBook Air',
         subtitle: 'C’è una nuova potenza nell’Air.',
-        url: 'macbook_air.jpg',
+        url: './img/macbook_air (1).jpg',
         price: 1299.00,
         category: 'laptop',
         description: 'Ti presentiamo il nuovo MacBook Air: il nostro portatile più sottile e leggero, completamente trasformato dal chip Apple M1. CPU fino a 3,5 volte più veloce. GPU fino a 5 volte più scattante. Il Neural Engine più evoluto di sempre, che assicura performance di machine learning fino a 9 volte migliori. Un’autonomia che su un MacBook Air non si era mai vista. E una tecnologia silenziosa, perché senza ventola. Ha una potenza senza precedenti, ed è pronto a seguirti ovunque.',
@@ -23,7 +23,7 @@ const products = [
         id: 3,
         title: 'MacBook Pro',
         subtitle: 'Potenza allo stato Pro.',
-        url: 'macbook_pro.jpg',
+        url: './img/macbook_pro (1).jpg',
         price: 1699.99,
         category: 'portable',
         description: 'Il MacBook Pro più potente di sempre è qui. Ha i velocissimi chip M1 Pro o M1 Max, i primi progettati da Apple per il mondo professionale, ed è capace di performance rivoluzionarie con un’incredibile durata della batteria. E poi ci sono un display Liquid Retina XDR spettacolare, il sistema audio e la videocamera migliori mai visti su un Mac, e tutte le porte che ti servono. MacBook Pro è un portatile di una nuova specie. Ed è un mostro di potenza.',
@@ -33,7 +33,7 @@ const products = [
         id: 22,
         title: 'iMac 24"',
         subtitle: 'Eccolo: di’ ciao al nuovo iMac.',
-        url: 'imac.png',
+        url: './img/imac.png',
         price: 1499.00,
         category: 'all-in-one',
         description: 'Ispirato al meglio di Apple, rivoluzionato dal chip M1. Si fa notare ovunque e si adatta perfettamente al tuo mondo.',
@@ -43,7 +43,7 @@ const products = [
         id: 245,
         title: 'Mac Pro',
         subtitle: 'Il potere di cambiare tutto.',
-        url: 'mac_pro.jpg',
+        url: './img/mac_pro (1).jpg',
         price: 3699.99,
         category: 'desktop',
         description: 'Nasce un Mac estremo, da ogni punto di vista. Con prestazioni, espansione e opzioni di configurazione al top, è il sistema dedicato agli utenti professionali che vogliono superare i limiti del possibile.',
@@ -53,7 +53,7 @@ const products = [
         id: 250,
         title: 'iPhone 13 Pro',
         subtitle: 'Super. Ultra. Pro.',
-        url: 'iphone.jpg',
+        url: './img/iphone.jpg',
         price: 1199.00,
         category: 'smartphone',
         description: 'Un nuovo, sorprendente salto di qualità per le fotocamere. Un display così reattivo che anche i gesti di sempre ti stupiranno. Il chip per smartphone più veloce al mondo. Robustezza eccezionale. E autonomia senza precedenti.',
@@ -63,7 +63,7 @@ const products = [
         id: 313,
         title: 'iPad',
         subtitle: 'Tuttofare. Tuttofacile',
-        url: 'ipad.png',
+        url: './img/ipad.png',
         price: 1999.99,
         category: 'tablet',
         description: 'Potente. Semplice da usare. Versatile. E progettato per fare tutto quello che ti piace. Con il nuovo iPad lavori, giochi, crei, impari e rimani sempre in contatto con chi vuoi. Ed è incredibile anche nel prezzo.',
@@ -73,10 +73,47 @@ const products = [
         id:457,
         title: 'iPhone 12',
         subtitle: 'Veloce che è una meraviglia.',
-        url: 'iphone12.jpg',
+        url: './img/iphone12.jpg',
         price: 1999.99,
         category: 'smartphone',
         description: 'Un chip superpotente. La velocità del 5G. Un sistema evoluto a doppia fotocamera. La resistenza del Ceramic Shield, più robusto di qualsiasi vetro per smartphone. E uno sfolgorante display OLED. iPhone 12 ha proprio tutto. Anche due formati perfetti.',
         io: ['wifi, USB-C, Jack cuffie', 'waterproof']
     }
 ]
+
+
+// section-4-text__variables
+/* let miniPicId = ['mini-pic-1', 'mini-pic-2', 'mini-pic-3', 'mini-pic-4', 'mini-pic-5']
+
+let miniPicImg = ['./img/playstation-5-thumb.png', './img/joypad-thumb.png', './img/headset-thumb.png', './img/remote-thumb.png', './img/camera-thumb.jpg'];
+
+let minPicTitle = ['Console PS5', 'Controller wireless DualSense', 'Cuffie wireless con microfono PULSE 3', 'Telecomando media', 'Telecamera HD']
+
+let minPicParagraph = ["Sperimenta la nuova generazione di incredibili giochi PlayStation." , "Vivi l'esperienza con un controller con feedback aptico, grilletti adattivi e microfono incorporato", "Goditi un'esperienza wireless perfetta con un paio di cuffie con microfono ottimizzate per l'audio 3D della console PS5.", "Controlla facilmente film, servizi di streaming e altro ancora sulla tua console PS5 grazie a un'interfaccia intuitiva.", "Partecipa in prima persona alle tue trasmissioni e ai video della tua azione di gioco grazie alla nitidissima acquisizione di immagini Full HD."]; */
+
+let bigPic = document.querySelector('.big-photo');
+let title = document.querySelector('.section-four-text > h2');
+let paragraph = document.querySelector('.section-four-text > p')
+let rowMiniPic = document.querySelector('.row-mini-pic-ps5');
+
+for(let i = 0; i < products.length; i++) {
+
+    rowMiniPic.innerHTML += `
+    <div id="${products[i].id}" class="col mb-3 text-center section-four-mini-pic" tabindex="0">
+        <img class="w-50 pb-1" src="${products[i].url}" alt="${products[i].title}">
+        <p class="d-none d-md-block pt-3">${products[i].title}</p>
+    </div>
+    `;
+}
+// click on mini-PS5
+for(let i = 0; i < products.length; i++) {
+    
+    let miniPic = document.getElementById(products[i].id)
+
+    miniPic.addEventListener('click', function(){
+
+    bigPic.src = `${products[i].url}`;
+    title.innerHTML = `${products[i].title}`;
+    paragraph.innerHTML = `${products[i].description}`
+    });
+}
